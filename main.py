3286a46f65d159df88aa8b32363164e8e5cc3679
BuildTools <unconfigured@null.spigotmc.org>
2022-10-10 14:44:11 +0200

    cleaned up main.py

--- main.py
+++ main.py
@@ -23,7 +23,3 @@ main()
 
 end = time.time()
 print("{:.2f}".format(round(end-start, 2)))
-
-
-a  =  """es ist ein Problem mit dem jeder PC und Mac Nutzer im Büro irgendwann konfrontiert wird ein wichtiger Brief soll als PDF gescannt werden oder ein abfotografiertes Dokument archiviert werden das Problem dabei der Scan oder das Foto allein sorgt zwar für eine digitale Kopie des Dokuments dass ich von Menschen den es doch sobald der Computer das Dokument in irgendeiner Form nutzen soll wird es knifflig soll der Text extrahiert oder der Inhalt der Datei indexiert werden stellt sich der Rechner an als wäre kein Text enthalten vor allem in Hinblick auf das papierlose Büro ist das ausgesprochen lästig weshalb es seit geraumer Zeit sogenannte Texterkennungssoftware gibt"""
-b = """es ist ein Problem mit dem Reeder PC und Mac Benutzer im Büro hört man konfrontiert wird ein wichtiger Brief soll als PDF getrennt werden oder ein abfotografierte Dokumente archiviert werden das Problem dabei der Scan oder das Foto allein sorgt für eine digitale Kopie des Dokuments dass sich von Menschen die lässt doch sobald der Computer das Dokument in irgendeiner Form sonst sollte hinschicken soll der Text extrahieren oder der Inhalt der Datei indexiert werden stellt nicht mehr Rechte an als wäre kein Text enthalten vor allem im Hinblick auf das papierlose Büro ist das ausgesprochen lästig weshalb es seit geraumer Zeit sogenannte Texterkennungssoftware gibt"""
